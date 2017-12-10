5bd703ecb591d3d04ba3b317434beaf481b1f99e
kanmani <kanmaniselvan@gmail.com>
2017-12-10 20:28:39 +0530

    Add methods which will save url with short_code in `/^[0-9a-zA-Z_]{2,6}$/ ` format with validations
    - Before save short_code will be checked for its format and it's not valid, system will automatically generate a valid short_code and save it.

--- app/models/shorten_url.rb
+++ app/models/shorten_url.rb
@@ -1,3 +1,32 @@
 class ShortenUrl < ApplicationRecord
+  SHORT_CODE_SAVE_REGEX = /^[0-9a-zA-Z_]{2,6}$/
 
+  validates :url, :short_code, presence: true
+  validates_uniqueness_of :short_code
+
+  before_save :validate_short_code
+
+  def has_valid_short_code?
+    self.short_code.to_s.match(SHORT_CODE_SAVE_REGEX)
+  end
+
+  # Returns uniq short_code which matches ^[0-9a-zA-Z_]{6}$ pattern
+  def generate_short_code
+    short_code = SecureRandom.urlsafe_base64(4)
+
+    # It's possible for the short_code to have '-' character or the short_code might already exist.
+    # On such cases, do the regeneration.
+    if short_code.match(SHORT_CODE_SAVE_REGEX).blank? || ShortenUrl.where(short_code: short_code).present?
+      return self.generate_short_code
+    end
+
+    short_code
+  end
+
+  private
+  def validate_short_code
+    return if self.has_valid_short_code?
+
+    self.short_code = self.generate_short_code
+  end
 end
