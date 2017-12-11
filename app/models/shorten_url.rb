cf5056aeb37efd6ee92407f9b2f91be588a6e655
kanmani <kanmaniselvan@gmail.com>
2017-12-11 22:56:54 +0530

    Add regex for URL pattern match and remove auto short_code generation.
    - Force the user to enter the short_code.

--- app/models/shorten_url.rb
+++ app/models/shorten_url.rb
@@ -1,28 +1,20 @@
 class ShortenUrl < ApplicationRecord
-  SHORT_CODE_SAVE_REGEX = /^[0-9a-zA-Z_]{2,6}$/
+  URL_REGEX = /https?:\/\/[\S]+/
+  SHORT_CODE_SAVE_REGEX = /^[0-9a-zA-Z]{2,6}$/
 
   belongs_to :user
 
   validates :url, :short_code, :user_id, presence: true
   validates_uniqueness_of :short_code
 
-  before_save :validate_short_code
+  validate :short_code_and_url
 
   def has_valid_short_code?
     self.short_code.to_s.match(SHORT_CODE_SAVE_REGEX)
   end
 
-  # Returns uniq short_code which matches ^[0-9a-zA-Z_]{6}$ pattern
-  def generate_short_code
-    short_code = SecureRandom.urlsafe_base64(4)
-
-    # It's possible for the short_code to have '-' character or the short_code might already exist.
-    # On such cases, do the regeneration.
-    if short_code.match(SHORT_CODE_SAVE_REGEX).blank? || ShortenUrl.where(short_code: short_code).present?
-      return self.generate_short_code
-    end
-
-    short_code
+  def has_valid_url?
+    self.url.to_s.match(URL_REGEX)
   end
 
   def update_last_seen_and_redirect_count
@@ -33,9 +25,13 @@ class ShortenUrl < ApplicationRecord
   end
 
   private
-  def validate_short_code
-    return if self.has_valid_short_code?
+  def short_code_and_url
+    unless self.has_valid_short_code?
+      errors.add(:short_code, "should be between 2 to 6 characters in any of [a-z], [A-Z], [0-9] characters")
+    end
 
-    self.short_code = self.generate_short_code
+    unless self.has_valid_url?
+      errors.add(:url, "is invalid. Valid URL ex: https://www.example.com")
+    end
   end
 end
